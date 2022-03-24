f58a35cfe3c23bd0efa3e08868d4ab2f907aa3b0
Luiz Fernando Amorim <nandoalpha@hotmail.com>
2022-03-24 16:06:57 -0300

    Contador

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
 function Home() {
-    return <h1>Página inicial</h1>
+    return  (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Clique para Adicionar</button>
+        </div>
+        )
 }
 
 export default Home
